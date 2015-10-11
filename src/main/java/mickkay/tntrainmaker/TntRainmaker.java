4766981d8eb7031e0b9d232c6569821a9fc81bdc
mickkay <mickkay>
2015-10-11 13:11:28 +0200

    Release 2.2.1

--- src/main/java/mickkay/tntrainmaker/TntRainmaker.java
+++ src/main/java/mickkay/tntrainmaker/TntRainmaker.java
@@ -23,7 +23,7 @@ public class TntRainmaker {
 
   public static final String NAME = "TNT-Rainmaker";
   public static final String ID = "TntRainmaker";
-  public static final String VERSION = "1.8-2.2.0";
+  public static final String VERSION = "1.8-2.2.1";
 
   public final Logger logger = LogManager.getLogger(TntRainmaker.class.getName());
   private final TntRain rain = new TntRain();
