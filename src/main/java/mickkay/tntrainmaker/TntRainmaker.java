25a4dcaed6474d518ef4eca9d9b170d171505235
mickkay <mickkay>
2016-07-03 15:32:36 +0200

    Upgrade to MC 1.9.4

--- src/main/java/mickkay/tntrainmaker/TntRainmaker.java
+++ src/main/java/mickkay/tntrainmaker/TntRainmaker.java
@@ -24,7 +24,7 @@ public class TntRainmaker {
 
   public static final String NAME = "TNT-Rainmaker";
   public static final String ID = "TntRainmaker";
-  public static final String VERSION = "1.9-2.4.0";
+  public static final String VERSION = "1.9.4-2.5.0";
 
   public final Logger logger = LogManager.getLogger(TntRainmaker.class.getName());
   private final TntRain rain = new TntRain();
